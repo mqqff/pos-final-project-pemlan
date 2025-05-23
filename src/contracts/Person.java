7bc29ae88b93f44daae7b583f37ca8f677dba919
Muhamad Nadhif Ramadhian Fahrurrozi <nadhiframadhian@student.ub.ac.id>
2025-05-23 23:47:54 +0700

    komit nadhif 1st

--- src/contracts/Person.java
+++ src/contracts/Person.java
@@ -13,9 +13,8 @@ public abstract class Person {
     private String name;
     private String phone;
     
-    public Person(String name, String phone) {
-        this.name = name;
-        this.phone = phone;
+    public Person() {
+       
     }
     
     public String getName() {
